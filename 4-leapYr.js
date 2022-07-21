f1d9ae63da4fe9019013fc1c9502b179b02dc50a
noman-swe <noman.swe130@gmail.com>
2022-07-21 07:58:16 +0600

    leap year updated

--- 4-leapYr.js
+++ 4-leapYr.js
@@ -11,3 +11,20 @@ console.log(leapYr);
 
 const myYr = leapYrCheck(2021);
 console.log(myYr);
+
+// 
+// program to check leap year
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+// take input
+const year = checkLeapYear(2100);
+
+console.log(year);
