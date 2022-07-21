80edeabaa9ec856dce61d577bde43b1432e59cc4
noman-swe <noman.swe130@gmail.com>
2022-07-21 08:30:53 +0600

    20-6 Recalculate factorial multiple times using a function

--- 5-factorial.js
+++ 5-factorial.js
@@ -4,3 +4,15 @@ for(var i = 1; i <= 7; i++){
     factorial = factorial * i;
 }
 console.log(factorial);
+
+// factorial with functions
+function factorialFunc(number){
+    let factor = 1;
+    for(let i = 1; i <= number; i++){
+        factor = factor * i;
+    }
+    return factor;
+}
+
+const factorialCheck = factorialFunc(7);
+console.log(factorialCheck);
